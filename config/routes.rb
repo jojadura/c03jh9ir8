c9d4f8b0391e2b6d560b2c55d80b4333e78c97e2
john j <as@gmail.com>
2015-11-24 14:51:14 -0500

    CCambio en routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
 
-  resources :actors , only: [:index,:new] 
+  #resources :actors , only: [:index,:new] 
+  get '/actors', to: 'actors#index'
+  get '/actors/new', to: 'actors#new' as "crear actor"
   post '/actors', to: 'actors#create'
 
 end
